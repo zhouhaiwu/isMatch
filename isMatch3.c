815de34b47d8854ef1b0ff7b1ee3fb16eb997b1a
zhou199611 <zhou199019901@163.com>
2021-01-06 20:18:52 +0800

    第一次提交

---
+++ isMatch3.c
@@ -0,0 +1,37 @@
+/*
+解题思路
+1、如果p为空，s为空匹配，s非空不匹配；
+2、s非空，p == s || p == '.'时第一个字符匹配；
+3、(p+1) != ''，则递归判断剩下的是否匹配 first_match && isMatch(++s, ++p)
+4、(p+1) == '*'，则有两种情况匹配：
+a: *匹配0个字符，s匹配剩下的，即isMatch(s, p+2)
+b: *匹配1个字符，继续用p匹配剩下的s，即first_match && isMatch(s+1, p)
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isMatch(char * s, char * p){
+    if (!*p) return !*s;
+    bool first_match = *s && (*s == *p || *p == '.');
+    if (*(p+1) == '*') {
+        return isMatch(s, p+2) || (first_match && isMatch(++s, p));
+    }
+    else {
+        return first_match && isMatch(++s, ++p);
+    }
+}
+
+int main() {
+    bool flag;
+    char *s = "";
+    char *p = ".";
+    //char *s = "a";
+    //char *p = ".*..a*";
+    flag = isMatch(s, p);
+    printf("flag:%d\n", flag);
+    return 0;
+}
+
+}
